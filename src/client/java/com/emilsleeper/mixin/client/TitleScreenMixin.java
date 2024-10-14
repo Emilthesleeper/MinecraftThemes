19816731673c00afd6955d382108e17e78f0fc91
Emilthesleeper <emilwege@icloud.com>
2024-10-14 16:58:51 +0200

    Added TitleScreenMixin

--- src/client/java/com/emilsleeper/mixin/client/TitleScreenMixin.java
+++ src/client/java/com/emilsleeper/mixin/client/TitleScreenMixin.java
@@ -2,13 +2,16 @@ package com.emilsleeper.mixin.client;
 
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
-import net.minecraft.client.gui.screen.world.SelectWorldScreen;
+import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
+import net.minecraft.client.gui.tooltip.Tooltip;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
 
 @Mixin(TitleScreen.class)
 public abstract class TitleScreenMixin extends Screen {
@@ -19,8 +22,8 @@ public abstract class TitleScreenMixin extends Screen {
 
     @Inject(at = @At("RETURN"), method = "initWidgetsNormal")
     private void addModsButton(int y, int spacingY, CallbackInfo ci) {
-        this.addDrawableChild(ButtonWidget.builder(Text.translatable("modname.customButton"), (button) -> {
-            this.client.setScreen(new SelectWorldScreen(this));
-        }).dimensions(this.width / 2 - 200 +252, y, 50, 200).build());
+        ButtonWidget testtext = this.addDrawableChild(ButtonWidget.builder(Text.translatable("modname.customButton"), (button) -> {
+            this.client.setScreen(new MultiplayerScreen(this));
+        }).dimensions(this.width / 2, y, this.width / 2, this.height / 2).build());
     }
 }
