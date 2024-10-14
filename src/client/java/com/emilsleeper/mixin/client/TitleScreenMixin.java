3e07b470460e62a78011db03b21145052f6ab97f
Emilthesleeper <emilwege@icloud.com>
2024-10-14 17:04:54 +0200

    Update TitleScreenMixin.java

--- src/client/java/com/emilsleeper/mixin/client/TitleScreenMixin.java
+++ src/client/java/com/emilsleeper/mixin/client/TitleScreenMixin.java
@@ -24,6 +24,6 @@ public abstract class TitleScreenMixin extends Screen {
     private void addModsButton(int y, int spacingY, CallbackInfo ci) {
         ButtonWidget testtext = this.addDrawableChild(ButtonWidget.builder(Text.translatable("modname.customButton"), (button) -> {
             this.client.setScreen(new MultiplayerScreen(this));
-        }).dimensions(this.width / 2, y, this.width / 2, this.height / 2).build());
+        }).dimensions(this.width / 2, y, this.width / 2, this.height / 2).tooltip(Tooltip.of(Text.translatable("test"))).build());
     }
 }
